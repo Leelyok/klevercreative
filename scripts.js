0af44cf98879fd1cce3479d61d375eb5d3c4c51b
Lily <liliya.zapasnaya@gmail.com>
2021-11-02 21:08:26 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 // Document . ready
 $(document).ready(function() {
-    $(".hamburger-button").click(function() {
-        $(this).toggleClass("active");
-        $(".mobile-menu").fadeToggle()
-    });
+    $('.hamburger-button').click(function(){
+        $('.mobile-menu').fadeToggle(100);
+        $(this).toggleClass('active');
+      });
+    
+      $('.mobile-menu a').click(function(){
+        $('.mobile-menu').fadeOut(100);
+        $(".hamburger-button").removeClass('active');
+      });
 
 
     //Animate scroll - activate
